416dd70612a2db000f9e0b83b1b67bfeece168bf
XRS0 <pya4kov@gmail.com>
2025-05-15 20:31:04 +0300

    changed from gemma to openchat

--- summarize_service/internal/container/container.go
+++ summarize_service/internal/container/container.go
@@ -1,17 +1,16 @@
 package container
 
 import (
-	"summarize_service/infrastructure/gemma"
+	"summarize_service/infrastructure/open_chat"
 	"summarize_service/usecase"
 )
 
 type AppContainer struct {
 	LLM usecase.LLMService
-	// другие зависимости
 }
 
 func BuildContainer() (*AppContainer, error) {
-	llm, err := gemma.NewGemmaApi()
+	llm, err := open_chat.NewOpenChatApi()
 	if err != nil {
 		return nil, err
 	}
