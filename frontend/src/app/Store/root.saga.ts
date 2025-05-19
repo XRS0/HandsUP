737839ee9bd1c647a2c7c9a6c6e31067f9243a71
YXUNGGG <s.krivostanenko@mail.ru>
2025-05-19 23:55:41 +0500

    change auth redux structure

--- frontend/src/app/Store/root.saga.ts
+++ frontend/src/app/Store/root.saga.ts
@@ -1,11 +1,10 @@
 import { all } from 'redux-saga/effects';
 import watchWsStatus from '@/entities/websocket/saga';
-import { watchFetchRegister } from '@/features/Register/models/saga';
+import { watchAuth } from '@/features/Auth';
 
 export default function* rootSaga() {
   yield all([
-    // webSocketSaga(), old
     watchWsStatus(),
-    watchFetchRegister(),
+    watchAuth(),
   ]);
 }
