5196a1881added32313cb78465f7c92ca7aa264f
YXUNGGG <s.krivostanenko@mail.ru>
2025-05-24 18:13:31 +0500

    fix token save

--- frontend/src/features/Auth/models/signUp.saga.ts
+++ frontend/src/features/Auth/models/signUp.saga.ts
@@ -5,11 +5,11 @@ import { AuthSliceActions } from "./slice";
 
 export function* fetchRegisterSaga({ payload }: ReturnType<typeof AuthSliceActions.fetchSignUpRequest>) {
   try {
-    const response: {message: string} & JWT = yield call(registerApiInstance, payload);
+    const response: {message: string, token: string} = yield call(registerApiInstance, payload);
     console.log(response);
 
-    yield localStorage.setItem("access_token", response.access_token);
-    yield put(AuthSliceActions.fetchSuccess(response.access_token));
+    yield localStorage.setItem("access_token", response.token);
+    yield put(AuthSliceActions.fetchSuccess(response.token));
   } catch (error: any) {
     console.error(error);
     yield put(AuthSliceActions.fetchFailure(error.message));
