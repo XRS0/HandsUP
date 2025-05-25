38a9ca7b582e3b6943add0644a1a58b744acee73
YXUNGGG <s.krivostanenko@mail.ru>
2025-05-26 02:38:58 +0500

    fixed md render 2

--- frontend/src/pages/main/containers/MDMessageBlock.tsx
+++ frontend/src/pages/main/containers/MDMessageBlock.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useRef, useState } from "react";
-import { useAppDispatch, useAppSelector } from "@/hooks/redux";
+import { useAppSelector } from "@/hooks/redux";
 import Markdown from "react-markdown";
 
 import "../ui/LoadedChat.scss";
@@ -8,17 +8,12 @@ import "@/features/ChatMessage/ui/StaticMessage.scss";
 
 const MDMessageBlock = () => {
   const { markdown } = useAppSelector(state => state.socket);
-  const dispatch = useAppDispatch();
+  // const dispatch = useAppDispatch();
 
   const [, updateState] = useState({});
   const forceUpdate = useCallback(() => updateState({}), []);
 
-  const messageRef = useRef<HTMLDivElement>(null);
-  const messageHeightRef = useRef<number | string>(0);
-  const lettersCountRef = useRef(0);
-
   const renderedMessage = useRef<string[]>([]);
-  const animationInterval = useRef<NodeJS.Timeout>(null);
   
   useEffect(() => {
     if (renderedMessage.current.join("") === markdown.join("")) return;
@@ -28,18 +23,9 @@ const MDMessageBlock = () => {
       : markdown;
 
     charsToRender.forEach((char, i) => {
-      if (messageRef.current) messageHeightRef.current = messageRef.current.offsetHeight;
       setTimeout(() => {
         const charLength = char.length;
-        
-        if ((charLength + lettersCountRef.current) >= 104) {
-          if (messageRef.current) {
-            messageHeightRef.current = +messageHeightRef.current + 23;
-          }
-          lettersCountRef.current = 0;
-        }
 
-        lettersCountRef.current += charLength;
         renderedMessage.current.push(char);
         forceUpdate();
       }, 100 * i);
