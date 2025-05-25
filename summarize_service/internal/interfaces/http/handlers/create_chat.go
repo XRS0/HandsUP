29b561bf21576e0ef02344ae7f63005ccfc1baae
XRS0 <pya4kov@gmail.com>
2025-05-25 13:42:41 +0300

    fixed create chat function

--- summarize_service/internal/interfaces/http/handlers/create_chat.go
+++ summarize_service/internal/interfaces/http/handlers/create_chat.go
@@ -1,16 +1,29 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/XRS0/HandsUp/summarize_service/internal/domain/models"
 	"github.com/gin-gonic/gin"
 )
 
 func (h *ChatHandler) CreateChat(c *gin.Context, topic string) {
-	chat := &models.Chat{
-		Topic: topic,
+	token, err := GetTokenFromHeader(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+	}
+	userID, err := ValidateToken(token, h.authClient)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+		return
 	}
 
-	_, err := h.chatService.CreateChat(chat)
+	chat := &models.Chat{
+		UserID: userID,
+		Topic:  topic,
+	}
+
+	_, err = h.chatService.CreateChat(chat)
 	if err != nil {
 		c.JSON(500, gin.H{"error": "Failed to delete chat"})
 		return
