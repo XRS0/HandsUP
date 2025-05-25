052f22ed193a0800178e2b2d7a909fb8ce6aeed3
YXUNGGG <s.krivostanenko@mail.ru>
2025-05-26 01:35:38 +0500

    added sending params into ws

--- frontend/src/pages/main/containers/MDMessageBlock.tsx
+++ frontend/src/pages/main/containers/MDMessageBlock.tsx
@@ -1,26 +1,60 @@
-import { useAppSelector } from "@/hooks/redux";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { useAppDispatch, useAppSelector } from "@/hooks/redux";
 import Markdown from "react-markdown";
 
-import "../ui/MainPage.scss";
+import "../ui/LoadedChat.scss";
 import "../ui/MDMessageBlock.scss";
 
 const MDMessageBlock = () => {
   const { markdown } = useAppSelector(state => state.socket);
+  const dispatch = useAppDispatch();
+  
+  const [, updateState] = useState({});
+  const forceUpdate = useCallback(() => updateState({}), []); // for updating
+  
+  const messageRef = useRef<HTMLDivElement>(null);
+  const messageHeightRef = useRef<number | string>(0);                 // height of message conntainer
+  const lettersCountRef = useRef(0);                  // for counting we sould height will changed
+  
+  const renderedMessage = useRef<string[]>([]);       // collecting already rendered words
+
+  useEffect(() => {
+    if (renderedMessage.current.join(" ") === markdown) return;
+
+    const wordsToRender = renderedMessage.current.length
+    ? markdown.split("").slice(renderedMessage.current.length)
+    : markdown.split("");
+
+    wordsToRender.map((word, i) => {
+      if (messageRef.current) messageHeightRef.current = messageRef.current.offsetHeight;
+      setTimeout(() => {
+        const wordLength = word.length + 1;                               // +1 for add space
+      
+        if ((wordLength + lettersCountRef.current) >= 105) {
+          if (messageRef.current) {
+            messageHeightRef.current = +messageHeightRef.current +  23;   // add some px for increase height
+          }
+          lettersCountRef.current = 0;                                    // reset letters count
+        }
+
+        lettersCountRef.current += wordLength;
+        renderedMessage.current.push(word);
+        forceUpdate();
+      }, 100 * i);
+    });
+  }, [markdown]);
 
   return (
-    <div className="conspect-message-block"
+    <div className="chat-message-container md-block --enter"
       style={{
-        position: "absolute",
-        zIndex: "5",
-        top: "3%",
-        left: "25%"
+        animation: 'fade-word 0.7s forwards cubic-bezier(0.11, 0, 0.5, 0)',
       }}
     >
-      <Markdown 
-       components={{h1: 'h2'}}    // Use h2s instead of h1s
-      >
-        {markdown}
-      </Markdown>
+      <div className="static-message">
+        <Markdown components={{h1: 'h2'}}>
+          {markdown}
+        </Markdown>
+      </div>
     </div>
   );
 }
