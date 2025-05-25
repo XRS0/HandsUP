4f02fd0b7fe014a8c219cfc38b923f1302b662c8
XRS0 <pya4kov@gmail.com>
2025-05-25 23:25:47 +0300

    fix login

--- account_service/internal/interfaces/http/dto/user.go
+++ account_service/internal/interfaces/http/dto/user.go
@@ -1,8 +1,8 @@
 package dto
 
 type User struct {
-	UserName string `json:"username" binding:"required"`
-	Email    string `json:"email" binding:"required,email"`
-	//TODO: add Balance
-	//TODO: add Topics (map[nameTopic]time)
+	UserName  string `json:"username" binding:"required"`
+	Email     string `json:"email" binding:"required,email"`
+	Balance   int    `json:"balance"`
+	PricePlan string `json:"price_plan"`
 }
