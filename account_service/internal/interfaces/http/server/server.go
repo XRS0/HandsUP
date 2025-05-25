93a4e5458dc1762ae20f6e709b6aa3cf9779e547
XRS0 <pya4kov@gmail.com>
2025-05-25 14:39:34 +0300

    cors fixed in account_service

--- account_service/internal/interfaces/http/server/server.go
+++ account_service/internal/interfaces/http/server/server.go
@@ -16,9 +16,6 @@ func NewHTTPServer(router *gin.Engine, authClient *auth.AuthClient) *HTTP_Server
 	if router == nil {
 		router = gin.Default()
 	}
-
-	router.Use(CORS())
-
 	return &HTTP_Server{
 		Router:     router,
 		AuthClient: authClient,
--- summarize_service/internal/interfaces/http/server.go
+++ summarize_service/internal/interfaces/http/server.go
@@ -14,10 +14,24 @@ type Server struct {
 
 func NewServer(cs service.ChatService, ac *auth.AuthClient) *Server {
 	router := gin.Default()
+	router.Use(CORS())
 	handler := handlers.NewChatHandler(cs, ac)
 	return &Server{
 		Router:  router,
 		handler: handler,
+	}
+}
+
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+			return
+		}
+		c.Next()
 	}
 }
 
